09c173d52526b874c401fd48bdabe21fc16fcab6
AndreyAgeev111 <62032780+AndreyAgeev111@users.noreply.github.com>
2022-02-18 00:52:51 +0300

    + add decorator instead function

--- src/receiver.py
+++ src/receiver.py
@@ -1,8 +1,38 @@
 from command import *
 
 
+def out_azure(text):
+    return "\033[36m {}\033[0m".format(text)
+
+
+def get_instructions(function):
+    def wrapped():
+        print(f'COMMANDS:'
+              f'\n {out_azure("add section")} --- add new section for expenses, '
+              f'{out_azure("arguments")}: name of section'
+              f'\n {out_azure("delete section")} --- delete one section, '
+              f'{out_azure("arguments")}: name of section'
+              f'\n {out_azure("delete expense")} --- delete expense with a comment and date, '
+              f'{out_azure("arguments")}: name of section, comment on expense, date of expense'
+              f'\n {out_azure("add expense")} --- add new expense in the section, '
+              f'{out_azure("arguments")}: name of section, price of expense, comment on expense'
+              f'\n {out_azure("clear section")} --- clear all expenses from, '
+              f'{out_azure("arguments")}: name of section'
+              f'\n {out_azure("get expenses from")} --- get expenses from one or more sections, '
+              f'{out_azure("arguments")}: name or names of sections'
+              f'\n {out_azure("get sections")} --- get all sections, {out_azure("arguments")}: none'
+              f'\n {out_azure("get chart")} --- get chart for all expenses, '
+              f'{out_azure("arguments")}: type of chart: bar or pie'
+              f'\n {out_azure("add to moneybox")} --- add money to your moneybox,'
+              f'{out_azure("arguments")}: money to add and comment'
+              f'\n {out_azure("check moneybox")} --- check balance of the moneybox, '
+              f'{out_azure("arguments")}: none')
+        function()
+    return wrapped
+
+
+@get_instructions
 def receive():
-    get_instructions()
     while True:
         print('Please enter the command')
         cmd = input("pysh >> ").strip()
@@ -23,30 +53,3 @@ def receive():
         except KeyError:
             print(f'{cmd} is unknown command!')
             print(' Try another:')
-
-
-def out_azure(text):
-    return "\033[36m {}".format(text)
-
-
-def get_instructions():
-    print(f'COMMANDS:'
-          f'\n {out_azure("add section")}\033[0m --- add new section for expenses, '
-          f'{out_azure("arguments")}\033[0m: name of section'
-          f'\n {out_azure("delete section")}\033[0m --- delete one section, '
-          f'{out_azure("arguments")}\033[0m: name of section'
-          f'\n {out_azure("delete expense")}\033[0m --- delete expense with a comment and date, '
-          f'{out_azure("arguments")}\033[0m: name of section, comment on expense, date of expense'
-          f'\n {out_azure("add expense")}\033[0m --- add new expense in the section, '
-          f'{out_azure("arguments")}\033[0m: name of section, price of expense, comment on expense'
-          f'\n {out_azure("clear section")}\033[0m --- clear all expenses from, '
-          f'{out_azure("arguments")}\033[0m: name of section'
-          f'\n {out_azure("get expenses from")}\033[0m --- get expenses from one or more sections, '
-          f'{out_azure("arguments")}\033[0m: name or names of sections'
-          f'\n {out_azure("get sections")}\033[0m --- get all sections, {out_azure("arguments")}\033[0m: none\033[0m'
-          f'\n {out_azure("get chart")}\033[0m --- get chart for all expenses, '
-          f'{out_azure("arguments")}\033[0m: type of chart: bar or pie'
-          f'\n {out_azure("add to moneybox")}\033[0m --- add money to your moneybox,'
-          f'{out_azure("arguments")}\033[0m: money to add and comment'
-          f'\n {out_azure("check moneybox")}\033[0m --- check balance of the moneybox, '
-          f'{out_azure("arguments")}\033[0m: none\033[0m')
